1c015accbc1d45998a541aa73f316a1e54a0e468
Maciej Pięta <maciej.pieta16@gmail.com>
2019-03-17 12:43:24 +0100

    My first Android App

---
+++ app/src/main/java/com/example/jopoty/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.jopoty;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void showAdultMenu (View view) {
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+    }
+    public void showKidsMenu (View view) {
+        Intent intent = new Intent(this, KidsActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
